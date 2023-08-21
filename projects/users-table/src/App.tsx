3b1d262475fbd71adf171872ec56e473b610258c
abizmo <abizmo@gmail.com>
2023-08-21 12:35:41 +0100

    feat(users table): fetch 100 rows

--- projects/users-table/src/App.tsx
+++ projects/users-table/src/App.tsx
@@ -1,13 +1,23 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App () {
-  return <>
-  <header>
-    <h1>Prueba técnica</h1>
-  </header>
-  <main>
-  </main>
-  </>
+function App() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=100')
+      .then((res) => res.json())
+      .then(({ results }) => setUsers(results))
+      .catch((error) => console.error(error))
+  }, [])
+
+  return (
+    <>
+      <header>
+        <h1>Prueba técnica</h1>
+      </header>
+      <main>{JSON.stringify(users)}</main>
+    </>
+  )
 }
 
 export default App
